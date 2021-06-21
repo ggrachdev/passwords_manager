adf06ee329787e7372f72200daa9eb9d61c8b154
ggrach <ggrachdev@yandex.ru>
2021-06-21 11:14:57 +0400

    Modify entity and users_api_get_all controller

--- src/Controller/Api/UsersApiController.php
+++ src/Controller/Api/UsersApiController.php
@@ -15,25 +15,33 @@ class UsersApiController extends AbstractController {
      */
     public function index(): Response {
         $apiResponse = new ApiResponse();
-        
+
         $em = $this->getDoctrine()->getManager();
         $userRepository = $em->getRepository(User::class);
         $usersDb = $userRepository->findAll();
 
-        if ($usersDb === null) {
-            $apiResponse->setFail();
-            $apiResponse->setErrors('Not found users');
-        } else {
-            $apiResponse->setSuccess();
-            $users = [];
-            
-            foreach ($usersDb as $user) {
-                $users[] = [
-                    'email' => $user->getEmail()
-                ];
+        if ($this->isGranted('ROLE_ADMIN')) {
+            if ($usersDb === null) {
+                $apiResponse->setFail();
+                $apiResponse->setErrors('Not found users');
+            } else {
+                $apiResponse->setSuccess();
+                $users = [];
+
+                foreach ($usersDb as $user) {
+                    $users[] = [
+                        'first_name' => $user->getFirstName(),
+                        'second_name' => $user->getSecondName(),
+                        'middle_name' => $user->getMiddleName(),
+                        'email' => $user->getEmail()
+                    ];
+                }
+
+                $apiResponse->setData(['users' => $users]);
             }
-            
-            $apiResponse->setData(['users' => $users]);
+        } else {
+            $apiResponse->setFail();
+            $apiResponse->setErrors('Has not access');
         }
 
         return $apiResponse->generate();
