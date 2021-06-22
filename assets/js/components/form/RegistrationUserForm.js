5c311f5f769f234d348ca74f897d03a16dae751e
ggrach <ggrachdev@yandex.ru>
2021-06-22 11:01:41 +0400

    Implemeted set roles in registration form

--- assets/js/components/form/RegistrationUserForm.js
+++ assets/js/components/form/RegistrationUserForm.js
@@ -63,31 +63,37 @@ export default class RegistrationUserForm extends Component {
                 <Form autoComplete="off" onSubmit={this.onSubmit}>
                     <Form.Input fluid 
                                 label="Email:" 
+                                required="true" 
                                 name="registration_user_form[email]" 
                                 type="email" 
                                 placeholder="Введите email" />
                     <Form.Input fluid 
                                 label="Имя:" 
+                                required="true" 
                                 name="registration_user_form[first_name]" 
                                 type="text" 
                                 placeholder="Введите имя" />
                     <Form.Input fluid 
                                 label="Фамилия:" 
+                                required="true" 
                                 name="registration_user_form[second_name]" 
                                 type="text" 
                                 placeholder="Введите фамилию" />
                     <Form.Input fluid 
                                 label="Отчество:" 
+                                required="true" 
                                 name="registration_user_form[middle_name]" 
                                 type="text" 
                                 placeholder="Введите отчество" />
                     <Form.Input fluid 
                                 label="Пароль для входа:" 
+                                required="true" 
                                 name="registration_user_form[password]" 
                                 type="text" 
                                 placeholder="Введите пароль" />
                     <Form.Input fluid 
                                 label="Повторите пароль:" 
+                                required="true" 
                                 name="registration_user_form[re_password]" 
                                 type="text" 
                                 placeholder="Повторите пароль" />
