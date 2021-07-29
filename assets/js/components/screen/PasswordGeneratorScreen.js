ca6b4b25ce431557d4842639911792ffdafc6141
ggrach <ggrachdev@yandex.ru>
2021-07-29 09:11:03 +0400

    Update PasswordGeneratorScreen.js

--- assets/js/components/screen/PasswordGeneratorScreen.js
+++ assets/js/components/screen/PasswordGeneratorScreen.js
@@ -1,99 +1,98 @@
 import React, { Component, useState } from 'react';
-import { Container, Header, Menu, Grid, Table, Icon } from 'semantic-ui-react';
+import { Container, Header, Menu, Grid, Table, Icon, Form } from 'semantic-ui-react';
+import PasswordGenerator from '../../src/PasswordGenerator/PasswordGenerator';
+import TextCopier from '../../src/TextCopier/TextCopier';
+import PasswordBreakingTimeCounter from '../../src/PasswordBreakingTimeCounter/PasswordBreakingTimeCounter';
+import Toasts from '../../src/Toasts/Toasts';
 
-function PasswordGenerator() {
+
+function PasswordGeneratorComponent() {
     const [password, setPassword] = useState('');
-    const [length, setLength] = useState(10);
+    const [length, setLength] = useState(15);
     const [isLowerCase, setIsLowerCase] = useState(true);
-    const [isUpperCase, setIsUpperCase] = useState(false);
-    const [isNumbers, setIsNumbers] = useState(false);
-    const [isSymbols, setIsSymbols] = useState(false);
+    const [isUpperCase, setIsUpperCase] = useState(true);
+    const [isNumbers, setIsNumbers] = useState(true);
+    const [isSymbols, setIsSymbols] = useState(true);
 
     const generatePassword = () => {
 
-        const pwd = 12345;
-        
-        setPassword(pwd);
+        if (length == 0 || length < 0)
+        {
+            Toasts.error('Нужно указать длину больше нуля');
+        } else if (length > 1000)
+        {
+            Toasts.error('Серьезно?');
+        } else
+        {
+            const pwd = PasswordGenerator.generate(
+                {
+                    needNumerics: isNumbers,
+                    needUpperChars: isUpperCase,
+                    needLowerChars: isLowerCase,
+                    needSymbols: isSymbols,
+                    length: length
+                }
+            );
+
+            setPassword(pwd);
+        }
+    };
+
+    if(password === '')
+    {
+        generatePassword();
     }
 
     return (
-        <div>
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <label>
-                            <span className="lbl-len">Длина:</span>
-                            <input
-                                type="number"
-                                className="input-len form-control"
-                                value={length}
-                                onChange={e => setLength(e.target.value)}
-                                />
-                        </label>
-                    </div>
+        <React.Fragment>
+            <Form>
+                <Form.Group widths='equal'>
+                    <Form.Input type="number" fluid label='Длина пароля' placeholder='Длина пароля' value={length} onChange={e => setLength(e.target.value)} />
+                </Form.Group>
+                <Form.Group inline>
+                    <label>Правила генерации:</label>
+                    <Form.Radio
+                        label='Маленькие буквы'
+                        value={true}
+                        checked={isLowerCase}
+                        onClick={() => setIsLowerCase(val => !val)}
+                        />
+                    <Form.Radio
+                        label='Большие буквы'
+                        value={true}
+                        checked={isUpperCase}
+                        onClick={() => setIsUpperCase(val => !val)}
+                        />
+                    <Form.Radio
+                        label='Числа'
+                        value={true}
+                        checked={isNumbers}
+                        onClick={() => setIsNumbers(val => !val)}
+                        />
+                    <Form.Radio
+                        label='Символы'
+                        value={true}
+                        checked={isSymbols}
+                        onClick={() => setIsSymbols(val => !val)}
+                        />
+                </Form.Group>
+        
+                <div>
+                    <b>Сгенерированный пароль:</b><br/>
+                    <Icon link color='grey' name='copy' onClick={() => {
+                            TextCopier.copy(password);
+                              }} /> {password} 
+                    <br/>
+                    <br/>
+                    <b>Пароль подбирается за:</b><br/>
+                    {PasswordBreakingTimeCounter.getTime(password)}
                 </div>
-                <div class="row">
-                    <div class="col">
-                        <label className="form-control">
-                            <input
-                                type="checkbox"
-                                className="mr-1"
-                                checked={isLowerCase}
-                                onChange={() => setIsLowerCase(val => !val)}
-                                />
-                            <span>Буквы нижнего регистра</span>
-                        </label>
-                    </div>
-                    <div class="col">
-                        <label className="form-control">
-                            <input
-                                type="checkbox"
-                                className="mr-1"
-                                checked={isUpperCase}
-                                onChange={() => setIsUpperCase(val => !val)}
-                                />
-                            <span>Буквы верхнего регистра</span>
-                        </label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <label className="form-control">
-                            <input
-                                type="checkbox"
-                                className="mr-1"
-                                checked={isNumbers}
-                                onChange={() => setIsNumbers(val => !val)}
-                                />
-                            <span>Использовать числа</span>
-                        </label>
-                    </div>
-                    <div class="col">
-                        <label className="form-control">
-                            <input
-                                type="checkbox"
-                                className="mr-1"
-                                checked={isSymbols}
-                                onChange={() => setIsSymbols(val => !val)}
-                                />
-                            <span>Использовать специальные символы</span>
-                        </label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <input
-                            type="button"
-                            className="btn btn-dark mt-2 mb-3"
-                            value="Generate Password"
-                            onClick={generatePassword} />
-                        <div>
-                            Сгенерированный пароль: {password}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        
+                <br/>
+        
+                <Form.Button primary onClick={generatePassword}>Сгенерировать</Form.Button>
+            </Form>
+        </React.Fragment>
         );
 }
 
@@ -101,9 +100,7 @@ export default class PasswordGeneratorScreen extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-
-        };
+        this.state = {};
     }
 
     render() {
@@ -111,8 +108,8 @@ export default class PasswordGeneratorScreen extends Component {
         return (
             <React.Fragment>
                 <Container>
-                    <Header as='h1'>Генерация паролей:</Header>
-                    <PasswordGenerator />
+                    <Header as='h1'>Генератор паролей:</Header>
+                    <PasswordGeneratorComponent />
                 </Container>
             </React.Fragment>
             );
